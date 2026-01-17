de54f7821217348c1448d2bdac0feab93b255da8
mahesha-br <maheshabr2004@gmail.com>
2026-01-18 01:52:24 +0530

    minor update

--- frontend_ai/src/utils/axios.js
+++ frontend_ai/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:"http://localhost:4000",
-    
-})
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+});
 
 export default instance;
