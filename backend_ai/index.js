93b90ab8a7a169d0f544b836915b6684e7d9ae21
mahesha-br <maheshabr2004@gmail.com>
2026-01-18 02:16:47 +0530

    minor change

--- backend_ai/index.js
+++ backend_ai/index.js
@@ -1,25 +1,32 @@
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config(); // ✅ dotenv
-
+const cors = require('cors')
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
+
+const path = require('path')
 
 require('./connection');
-
 app.use(express.json());
-
 app.use(cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL
-}));
+    credentials:true,
+    origin:"http://localhost:5173"
+}))
 
 const UserRoutes = require('./Routes/user');
 const ResumeRoutes = require('./Routes/resume');
 
-app.use('/api/user', UserRoutes);
-app.use('/api/resume', ResumeRoutes);
+app.use('/api/user',UserRoutes)
+app.use('/api/resume',ResumeRoutes)
 
-app.listen(PORT, () => {
-    console.log("Backend is running on port", PORT);
-});
+
+// // Serve static files from the build folder
+// app.use(express.static(path.join(__dirname, "build")));
+
+// // Catch-all route: send index.html for React Router
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
+app.listen(PORT,()=>{
+    console.log("backend is running on port",PORT)
+})
