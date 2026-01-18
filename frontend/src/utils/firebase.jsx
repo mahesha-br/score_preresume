8023dd96273b61b82ca223ad8159d98b5230a629
mahesha-br <maheshabr2004@gmail.com>
2026-01-18 12:30:46 +0530

    vite api error change

--- frontend/src/utils/firebase.jsx
+++ frontend/src/utils/firebase.jsx
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTH_DOMIAN,
-  projectId: process.env.PORJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGEING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.measurementId
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 
